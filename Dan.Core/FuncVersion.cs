338e323ea43bb91914a11f11ceec97f0fb1127bd
Bjørn Dybvik Langfors <bdl@digdir.no>
2022-09-05 18:38:12 +0200

    Fix version endpoint

--- Dan.Core/FuncVersion.cs
+++ Dan.Core/FuncVersion.cs
@@ -1,67 +1,60 @@
+using System.Globalization;
 using System.Net;
 using System.Reflection;
 using Dan.Core.Attributes;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 
-namespace Dan.Core
+namespace Dan.Core;
+
+/// <summary>
+/// Class for function showing version information
+/// </summary>
+public class FuncVersion
 {
     /// <summary>
-    /// Class for function showing version information
+    /// Endpoint showing version information
     /// </summary>
-    public class FuncVersion
+    /// <param name="req">The request</param>
+    /// <returns>The response</returns>
+    [Function("version"), NoAuthentication]
+    public async Task<HttpResponseData> RunAsync([HttpTrigger(AuthorizationLevel.Anonymous, "get")]HttpRequestData req)
     {
-        /// <summary>
-        /// Endpoint showing version information
-        /// </summary>
-        /// <param name="req">The request</param>
-        /// <returns>The response</returns>
-        [Function("version"), NoAuthentication]
-        public async Task<HttpResponseData> RunAsync([HttpTrigger(AuthorizationLevel.Anonymous, "get")]HttpRequestData req)
+        var assembly = Assembly.GetExecutingAssembly();
+        var output = new
         {
-            var assembly = Assembly.GetExecutingAssembly();
-            var output = new
+            name = assembly.GetName().Name,
+            built = GetBuildDate(assembly).ToString("u"),
+            commit = ThisAssembly.Git.Commit
+        };
+
+        var response = req.CreateResponse();
+        response.StatusCode = HttpStatusCode.OK;
+        await response.WriteAsJsonAsync(output);
+
+        return response;
+    }
+
+
+    private static DateTime GetBuildDate(Assembly assembly)
+    {
+        const string buildVersionMetadataPrefix = "+build";
+
+        var attribute = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>();
+        if (attribute?.InformationalVersion != null)
+        {
+            var value = attribute.InformationalVersion;
+            var index = value.IndexOf(buildVersionMetadataPrefix, StringComparison.Ordinal);
+            if (index > 0)
             {
-                name = assembly.GetName().Name,
-                version = assembly.GetName().Version?.ToString(),
-                built = GetLinkerTimestampUtc(assembly).ToString("u"),
-                commit = GetGitHash(assembly)
-            };
-
-            var response = req.CreateResponse();
-            response.StatusCode = HttpStatusCode.OK;
-            await response.WriteAsJsonAsync(output);
-
-            return response;
-        }
-
-        private static DateTime GetLinkerTimestampUtc(Assembly assembly)
-        {
-            var location = assembly.Location;
-            return GetLinkerTimestampUtc(location);
-        }
-
-        private static DateTime GetLinkerTimestampUtc(string filePath)
-        {
-            const int peHeaderOffset = 60;
-            const int linkerTimestampOffset = 8;
-            var bytes = new byte[2048];
-
-            using (var file = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
-            {
-                var unused = file.Read(bytes, 0, bytes.Length);
+                value = value[(index + buildVersionMetadataPrefix.Length)..];
+                if (DateTime.TryParseExact(value, "yyyyMMddHHmmss", CultureInfo.InvariantCulture, DateTimeStyles.None, out var result))
+                {
+                    return result;
+                }
             }
-
-            var headerPos = BitConverter.ToInt32(bytes, peHeaderOffset);
-            var secondsSince1970 = BitConverter.ToInt32(bytes, headerPos + linkerTimestampOffset);
-            var dt = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-            return dt.AddSeconds(secondsSince1970);
         }
 
-        private static string? GetGitHash(Assembly assembly)
-        {
-            var attrs = assembly.GetCustomAttributes<AssemblyMetadataAttribute>();
-            return attrs.FirstOrDefault(a => a.Key == "GitHash")?.Value;
-        }
+        return default;
     }
 }
