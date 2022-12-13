13afed48cd2d41a85f296c91bbda71d1134d011a
Mark Cilia Vincenti <markciliavincenti@gmail.com>
2022-12-13 13:10:29 +0100

    Re-implemented KeyedLock for backwards compatibility

---
+++ Dan.Common/Util/KeyedLock.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dan.Common.Util
+{
+    internal class KeyedLock
+    {
+    }
+}
