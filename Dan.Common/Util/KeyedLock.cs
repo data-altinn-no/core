448794c6781b87e790f4f96c2839e66f00a78d13
Mark Cilia Vincenti <markciliavincenti@gmail.com>
2022-12-13 14:08:20 +0100

    Restored KeyedLock

--- Dan.Common/Util/KeyedLock.cs
+++ Dan.Common/Util/KeyedLock.cs
@@ -1,12 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AsyncKeyedLock;
 
-namespace Dan.Common.Util
+namespace Dan.Common.Util;
+
+public class KeyedLock<TKey> where TKey : notnull
 {
-    internal class KeyedLock
+    private readonly AsyncKeyedLocker<TKey> _asyncKeyedLocker;
+
+    public KeyedLock(IEqualityComparer<TKey>? keyComparer = null, int poolCapacity = 10)
     {
+        _asyncKeyedLocker = (keyComparer == null) ? new(new AsyncKeyedLockOptions(poolSize: poolCapacity)) : new(new AsyncKeyedLockOptions(poolSize: poolCapacity), keyComparer);
     }
+
+    public async Task<bool> WaitAsync(TKey key, int millisecondsTimeout,
+        CancellationToken cancellationToken = default)
+    {
+        var releaser = _asyncKeyedLocker.GetOrAdd(key);
+        bool entered = false;
+        try
+        {
+            entered = await releaser.SemaphoreSlim.WaitAsync(millisecondsTimeout,
+                cancellationToken).ConfigureAwait(false);
+        }
+        finally
+        {
+            if (!entered) releaser.Dispose();
+        }
+
+        return entered;
+    }
+
+    public Task WaitAsync(TKey key, CancellationToken cancellationToken = default)
+        => WaitAsync(key, Timeout.Infinite, cancellationToken);
+
+    public bool Wait(TKey key, int millisecondsTimeout,
+        CancellationToken cancellationToken = default)
+    {
+        var releaser = _asyncKeyedLocker.GetOrAdd(key);
+        bool entered = false;
+        try
+        {
+            entered = releaser.SemaphoreSlim.Wait(millisecondsTimeout, cancellationToken);
+        }
+        finally
+        {
+            if (!entered) releaser.Dispose();
+        }
+
+        return entered;
+    }
+
+    public void Wait(TKey key, CancellationToken cancellationToken = default)
+        => Wait(key, Timeout.Infinite, cancellationToken);
 }
