7b7f5cab53d04ae12ad5b6e36483d31883de9fca
Mark Cilia Vincenti <markciliavincenti@gmail.com>
2022-12-17 07:38:27 +0100

    Like-for-like replacement of KeyedLock with AsyncKeyedLock library. (#21)
    * Like-for-like replacement of KeyedLock with AsyncKeyedLock library.
    
    * Update to AsyncKeyedLock 5.0.4
    
    * Update to AsyncKeyedLock 5.1.0
    
    * Update to AsyncKeyedLock 5.1.1
    
    * Re-implemented KeyedLock for backwards compatibility
    
    * Restored KeyedLock
    
    * Added Obsolete attribute to KeyedLock
    
    * Update to AsyncKeyedLock 5.1.2

--- Dan.Common/Util/KeyedLock.cs
+++ Dan.Common/Util/KeyedLock.cs
@@ -1,34 +1,30 @@
-﻿using System.Diagnostics;
+﻿using AsyncKeyedLock;
 
 namespace Dan.Common.Util;
 
-// Based on https://stackoverflow.com/questions/31138179/asynchronous-locking-based-on-a-key/65256155#65256155
+[Obsolete("Please use AsyncKeyedLocker<T> instead.")]
 public class KeyedLock<TKey> where TKey : notnull
 {
-    private readonly Dictionary<TKey, (SemaphoreSlim, int)> _perKey;
-    private readonly Stack<SemaphoreSlim> _pool;
-    private readonly int _poolCapacity;
+    private readonly AsyncKeyedLocker<TKey> _asyncKeyedLocker;
 
     public KeyedLock(IEqualityComparer<TKey>? keyComparer = null, int poolCapacity = 10)
     {
-        _perKey = new Dictionary<TKey, (SemaphoreSlim, int)>(keyComparer);
-        _pool = new Stack<SemaphoreSlim>(poolCapacity);
-        _poolCapacity = poolCapacity;
+        _asyncKeyedLocker = (keyComparer == null) ? new(new AsyncKeyedLockOptions(poolSize: poolCapacity)) : new(new AsyncKeyedLockOptions(poolSize: poolCapacity), keyComparer);
     }
 
     public async Task<bool> WaitAsync(TKey key, int millisecondsTimeout,
         CancellationToken cancellationToken = default)
     {
-        var semaphore = GetSemaphore(key);
+        var releaser = _asyncKeyedLocker.GetOrAdd(key);
         bool entered = false;
         try
         {
-            entered = await semaphore.WaitAsync(millisecondsTimeout,
+            entered = await releaser.SemaphoreSlim.WaitAsync(millisecondsTimeout,
                 cancellationToken).ConfigureAwait(false);
         }
         finally
         {
-            if (!entered) ReleaseSemaphore(key, entered: false);
+            if (!entered) releaser.Dispose();
         }
 
         return entered;
@@ -40,15 +36,15 @@ public class KeyedLock<TKey> where TKey : notnull
     public bool Wait(TKey key, int millisecondsTimeout,
         CancellationToken cancellationToken = default)
     {
-        var semaphore = GetSemaphore(key);
+        var releaser = _asyncKeyedLocker.GetOrAdd(key);
         bool entered = false;
         try
         {
-            entered = semaphore.Wait(millisecondsTimeout, cancellationToken);
+            entered = releaser.SemaphoreSlim.Wait(millisecondsTimeout, cancellationToken);
         }
         finally
         {
-            if (!entered) ReleaseSemaphore(key, entered: false);
+            if (!entered) releaser.Dispose();
         }
 
         return entered;
@@ -56,58 +52,4 @@ public class KeyedLock<TKey> where TKey : notnull
 
     public void Wait(TKey key, CancellationToken cancellationToken = default)
         => Wait(key, Timeout.Infinite, cancellationToken);
-
-    public void Release(TKey key) => ReleaseSemaphore(key, entered: true);
-
-    private SemaphoreSlim GetSemaphore(TKey key)
-    {
-        SemaphoreSlim? semaphore;
-        lock (_perKey)
-        {
-            if (_perKey.TryGetValue(key, out var entry))
-            {
-                (semaphore, var counter) = entry;
-                _perKey[key] = (semaphore, ++counter);
-            }
-            else
-            {
-                lock (_pool) semaphore = _pool.Count > 0 ? _pool.Pop() : null;
-                semaphore ??= new SemaphoreSlim(1, 1);
-                _perKey[key] = (semaphore, 1);
-            }
-        }
-
-        return semaphore;
-    }
-
-    private void ReleaseSemaphore(TKey key, bool entered)
-    {
-        SemaphoreSlim semaphore;
-        int counter;
-        lock (_perKey)
-        {
-            if (_perKey.TryGetValue(key, out var entry))
-            {
-                (semaphore, counter) = entry;
-                counter--;
-                if (counter == 0)
-                    _perKey.Remove(key);
-                else
-                    _perKey[key] = (semaphore, counter);
-            }
-            else
-            {
-                throw new InvalidOperationException("Key not found.");
-            }
-        }
-
-        if (entered) semaphore.Release();
-        if (counter == 0)
-        {
-            Debug.Assert(semaphore.CurrentCount == 1);
-            lock (_pool)
-                if (_pool.Count < _poolCapacity)
-                    _pool.Push(semaphore);
-        }
-    }
 }
