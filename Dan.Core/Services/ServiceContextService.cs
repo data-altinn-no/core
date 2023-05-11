c4b52197622540bfafd4cfb5031024d547630514
erlend oksvoll <erlend.oksvoll@digdir.no>
2023-05-11 13:34:47 +0200

    added due diligence product

--- Dan.Core/Services/ServiceContextService.cs
+++ Dan.Core/Services/ServiceContextService.cs
@@ -207,6 +207,22 @@ public class ServiceContextService : IServiceContextService
                     }
                 }
             },
+            new ServiceContext()
+            {
+                Name = "eDueDiligence",
+                Id = "duediligence-product",
+                ValidLanguages = new List<string>() {Constants.LANGUAGE_CODE_NORWEGIAN_NB },
+                AuthorizationRequirements = new List<Requirement>() {
+                    new MaskinportenScopeRequirement()
+                    {
+                        RequiredScopes = new List<string>() { "altinn:dataaltinnno/duediligence" }
+                    },
+                    new AccreditationPartyRequirement()
+                    {
+                        PartyRequirements = new List<AccreditationPartyRequirementType>() { AccreditationPartyRequirementType.RequestorAndOwnerAreEqual }
+                    }
+                }
+            },
         };
 
         return await Task.FromResult(serviceContexts);
