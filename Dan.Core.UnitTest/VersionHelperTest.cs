16e47d2404fa6ce28db31b5528119645a2efbf61
Bjørn Dybvik Langfors <bdl@digdir.no>
2023-05-19 12:38:16 +0200

    Update dependencies, add test for versionhelper

---
+++ Dan.Core.UnitTest/VersionHelperTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Dan.Core.Helpers;
+
+namespace Dan.Core.UnitTest
+{
+    [TestClass]
+    [ExcludeFromCodeCoverage]
+    public class VersionHelperTest
+    {
+        [TestMethod]
+        public void VersionHelperReturnsValidData()
+        {
+            var versionInfo = VersionHelper.GetVersionInfo();
+
+            Assert.IsNotNull(versionInfo);
+            Assert.IsNotNull(versionInfo.Name);
+            Assert.IsNotNull(versionInfo.Built);
+            Assert.IsNotNull(versionInfo.Commit);
+            Assert.IsNotNull(versionInfo.CommitDate);
+        }
+    }
+}
