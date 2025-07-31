28dc90ab22eac1d716b837369c18caa4efe56f52
Erlend Oksvoll <erlend.oksvoll@digdir.no>
2025-07-31 14:35:34 +0200

    added memcache service for use in plugins to common

---
+++ Dan.Common/Interfaces/IPluginMemoryCacheProvider.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Dan.Common.Models;
+
+namespace Dan.Common.Interfaces;
+
+    public interface IPluginMemoryCacheProvider
+    {
+        public Task<(bool success, T result)> TryGet<T>(string key);
+
+        public Task<T> SetCache<T>(string key, T model, TimeSpan timeToLive);
+    }
+
---
+++ Dan.Common/Services/PluginMemoryCacheProviderService.cs
@@ -0,0 +1,36 @@
+﻿using Dan.Common.Interfaces;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace Dan.Common.Services
+
+{
+    public class PluginMemoryCacheProvider : IPluginMemoryCacheProvider
+    {
+        private readonly IMemoryCache _memoryCache;
+
+        public PluginMemoryCacheProvider(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;            
+        }
+        public Task<(bool success, T result)> TryGet<T>(string key)
+        {
+            bool success = _memoryCache.TryGetValue(key, out T result);
+            return Task.FromResult<(bool, T)>((success, result));
+        }
+
+        public async Task<T> SetCache<T>(string key, T value, TimeSpan timeToLive)
+        {
+            MemoryCacheEntryOptions cacheEntryOptions = new MemoryCacheEntryOptions()
+            {
+                Priority = CacheItemPriority.High
+            };
+
+            cacheEntryOptions.SetAbsoluteExpiration(timeToLive);
+            var result = _memoryCache.Set(key, value, cacheEntryOptions);
+
+            await Task.CompletedTask;
+
+            return result;
+        }
+    }
+}
